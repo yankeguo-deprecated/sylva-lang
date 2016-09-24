b2a50bf8a22958533e8436cb6adc369fc91630c5
Ryan Wade <ryan.wade@protonmail.com>
2016-09-24 16:06:09 +0800

    add sylva_func resolving and invocation

--- include/sylva/runtime.h
+++ include/sylva/runtime.h
@@ -7,6 +7,8 @@
 
 #include "sylva/platform.h"
 
+#include <stdarg.h>
+
 __CPP_DECL_START
 
 ///////////////// Primary Types  //////////////////
@@ -101,22 +103,53 @@ typedef struct {
   };
 } sylva_value;
 
-//////////////// Func  /////////////////////////
+#define sylva_value_nil ((sylva_value){ .type = sylva_value_type_nil, .integer_value = 0 })
+#define sylva_value_boolean(X) ((sylva_value){ .type = sylva_value_type_boolean, .integer_value = (X) != 0 })
+#define sylva_value_integer(X) ((sylva_value){ .type = sylva_value_type_boolean, .integer_value = (X)})
+#define sylva_value_float(X) ((sylva_value){ .type = sylva_value_type_float, .float_value = (X)})
+#define sylva_value_object(X) ((sylva_value){ .type = sylva_value_type_object, .object_value = (X)})
+#define sylva_value_class(X) ((sylva_value){ .type = sylva_value_type_class, .class_value = (X)})
+#define sylva_value_module(X) ((sylva_value){ .type = sylva_value_type_module, .module_value = (X)})
+
+//////////////// Func Arguments /////////////////////////
 
 typedef struct {
-  sylva_index capacity;
+  sylva_index length;
   sylva_value *values;
 } sylva_args;
 
-#define sylva_args_empty ((sylva_args){ .capacity = 0, .values = NULL })
+#define sylva_args_empty ((sylva_args){ .length = 0, .values = NULL })
 
 typedef sylva_args *sylva_args_ref;
 
 SYLVA_EXTERN sylva_value sylva_args_get(sylva_args args, sylva_index index);
 
-typedef sylva_value (*sylva_func)(sylva_value self, sylva_args args);
+SYLVA_EXTERN sylva_args sylva_args_create(sylva_index length, ...);
+
+SYLVA_EXTERN sylva_args sylva_args_v_create(sylva_index length, va_list list);
+
+SYLVA_EXTERN void sylva_args_destroy(sylva_args args);
+
+////////////////     Func     /////////////////////////
+
+/**
+ * sylva_func is the general type for sylva functions
+ *
+ * for instance methods, context will be a sylva_object_ref, for static, it will be sylva_class_ref
+ */
+typedef sylva_value (*sylva_func)(sylva_value context, sylva_args args);
 
 typedef sylva_index sylva_func_id;
+
+/**
+ * call a sylva_func
+ */
+SYLVA_EXTERN sylva_value sylva_func_call(sylva_func func, sylva_value context, sylva_index length, ...);
+
+/**
+ * call a sylva_func with va_list as argument
+ */
+SYLVA_EXTERN sylva_value sylva_func_v_call(sylva_func func, sylva_value context, sylva_index length, va_list list);
 
 enum {
   sylva_func_id_not_found = sylva_index_not_found,
@@ -128,7 +161,7 @@ enum {
  * In an ideal situation, sylva_func_list should be initialized literally
  */
 struct sylva_func_list_t {
-  sylva_index capacity;
+  sylva_index length;
   sylva_func_id *func_ids;
   sylva_func *funcs;
 };
@@ -150,14 +183,36 @@ SYLVA_EXTERN sylva_boolean sylva_func_list_exists(sylva_func_list list, sylva_fu
  * @param list sylva_func_list
  * @param func_id sylva_func_id
  * @param func sylva_func or NULL
- * @return NULL if out of capacity, func if succeeded
+ * @return NULL if out of length, func if succeeded
  */
 SYLVA_EXTERN sylva_func sylva_func_list_set(sylva_func_list list, sylva_func_id func_id, sylva_func func);
+
+SYLVA_EXTERN sylva_func sylva_class_instance_func_resolve(sylva_class class, sylva_func_id func_id);
+
+SYLVA_EXTERN sylva_func sylva_class_static_func_resolve(sylva_class class, sylva_func_id func_id);
+
+/**
+ * resolve a sylva_func from sylva_func_id and a context
+ *
+ * !!WARN!!, this will not agressively resolve instance func to static func
+ *
+ */
+SYLVA_EXTERN sylva_func sylva_func_resolve(sylva_value context, sylva_func_id func_id);
+
+/**
+ * resolve a sylva_func with context, and invoke it
+ */
+SYLVA_EXTERN sylva_value sylva_call(sylva_value context, sylva_func_id func_id, sylva_index length, ...);
+
+/**
+ * resolve a sylva_func with context, and invoke it, va_list version
+ */
+SYLVA_EXTERN sylva_value sylva_v_call(sylva_value context, sylva_func_id func_id, sylva_index length, va_list list);
 
 ///////////////////// FuncId Registry /////////////////
 
 typedef struct {
-  sylva_index capacity;
+  sylva_index length;
   sylva_func_id *func_ids;
   char **func_names;
 } sylva_func_id_registry;
@@ -188,12 +243,12 @@ enum {
 };
 
 struct sylva_member_list_t {
-  sylva_index capacity;
+  sylva_index length;
   sylva_member_id *member_ids;
 };
 
 struct sylva_members_t {
-  sylva_index capacity;
+  sylva_index length;
   sylva_member_id *member_ids;
   sylva_value *member_values;
 };
@@ -201,7 +256,7 @@ struct sylva_members_t {
 //////////////////// MemberId Registry /////////////////
 
 typedef struct {
-  sylva_index capacity;
+  sylva_index length;
   sylva_member_id *member_ids;
   char **member_names;
 } sylva_member_id_registry;
--- src/runtime/runtime.c
+++ src/runtime/runtime.c
@@ -10,7 +10,7 @@
 #include <stdarg.h>
 
 sylva_value sylva_args_get(sylva_args args, sylva_index index) {
-  if (args.capacity > index) {
+  if (args.length > index) {
     return args.values[index];
   }
   return (sylva_value) {
@@ -19,8 +19,47 @@ sylva_value sylva_args_get(sylva_args args, sylva_index index) {
   };
 }
 
+SYLVA_EXTERN sylva_args sylva_args_create(sylva_index length, ...) {
+  va_list list;
+  va_start(list, length);
+  sylva_args args = sylva_args_v_create(length, list);
+  va_end(list);
+  return args;
+}
+
+sylva_args sylva_args_v_create(sylva_index length, va_list list) {
+  sylva_args args = sylva_args_empty;
+
+  if (length > 0) {
+    args.values = malloc(sizeof(sylva_value) * length);
+    for (sylva_index i = 0; i < length; i++) {
+      args.values[i] = va_arg(list, sylva_value);
+    }
+  }
+
+  return args;
+}
+
+void sylva_args_destroy(sylva_args args) {
+  if (args.values != NULL) {
+    free(args.values);
+  }
+}
+
+sylva_value sylva_func_call(sylva_func func, sylva_value context, sylva_index length, ...) {
+  va_list list;
+  va_start(list, length);
+  sylva_value result = sylva_func_v_call(func, context, length, list);
+  va_end(list);
+  return result;
+}
+
+sylva_value sylva_func_v_call(sylva_func func, sylva_value context, sylva_index length, va_list list) {
+  return func(context, sylva_args_v_create(length, list));
+}
+
 sylva_boolean sylva_func_list_exists(sylva_func_list list, sylva_func_id func_id) {
-  for (sylva_index i = 0; i < list.capacity; i++) {
+  for (sylva_index i = 0; i < list.length; i++) {
     if (list.func_ids[i] == func_id) {
       return sylva_true;
     }
@@ -29,7 +68,7 @@ sylva_boolean sylva_func_list_exists(sylva_func_list list, sylva_func_id func_id
 }
 
 sylva_func sylva_func_list_get(sylva_func_list list, sylva_func_id func_id) {
-  for (sylva_index i = 0; i < list.capacity; i++) {
+  for (sylva_index i = 0; i < list.length; i++) {
     if (list.func_ids[i] == func_id)
       return list.funcs[i];
   }
@@ -37,7 +76,7 @@ sylva_func sylva_func_list_get(sylva_func_list list, sylva_func_id func_id) {
 }
 
 sylva_func sylva_func_list_set(sylva_func_list list, sylva_func_id func_id, sylva_func func) {
-  for (sylva_index i = 0; i < list.capacity; i++) {
+  for (sylva_index i = 0; i < list.length; i++) {
     if (list.func_ids[i] == func_id || list.func_ids[0] == 0) {
       list.func_ids[i] = func_id;
       list.funcs[i] = func;
@@ -46,8 +85,76 @@ sylva_func sylva_func_list_set(sylva_func_list list, sylva_func_id func_id, sylv
   return NULL;
 }
 
+sylva_func sylva_class_instance_func_resolve(sylva_class class, sylva_func_id func_id) {
+  // search class instance func
+  sylva_func instance_func = sylva_func_list_get(*class.instance_func_list, func_id);
+  if (instance_func != NULL) {
+    return instance_func;
+  }
+  // search module instance func
+  for (sylva_index i = 0; i < class.modules_count; i++) {
+    sylva_module module = class.modules[i];
+    instance_func = sylva_func_list_get(*module.instance_func_list, func_id);
+    if (instance_func != NULL) {
+      return instance_func;
+    }
+  }
+  // walk to super
+  if (class.super != NULL) {
+    return sylva_class_instance_func_resolve(*class.super, func_id);
+  }
+  return NULL;
+}
+
+sylva_func sylva_class_static_func_resolve(sylva_class class, sylva_func_id func_id) {
+  // search class instance func
+  sylva_func instance_func = sylva_func_list_get(*class.static_func_list, func_id);
+  if (instance_func != NULL) {
+    return instance_func;
+  }
+  // search module instance func
+  for (sylva_index i = 0; i < class.modules_count; i++) {
+    sylva_module module = class.modules[i];
+    instance_func = sylva_func_list_get(*module.static_func_list, func_id);
+    if (instance_func != NULL) {
+      return instance_func;
+    }
+  }
+  // walk to super
+  if (class.super != NULL) {
+    return sylva_class_instance_func_resolve(*class.super, func_id);
+  }
+  return NULL;
+}
+
+sylva_func sylva_func_resolve(sylva_value context, sylva_func_id func_id) {
+  if (context.type == sylva_value_type_object) {
+    return sylva_class_instance_func_resolve(*context.object_value->class, func_id);
+  }
+  if (context.type == sylva_value_type_class) {
+    return sylva_class_static_func_resolve(*context.class_value, func_id);
+  }
+  return NULL;
+}
+
+sylva_value sylva_call(sylva_value context, sylva_func_id func_id, sylva_index length, ...) {
+  va_list list;
+  va_start(list, length);
+  sylva_value result = sylva_v_call(context, func_id, length, list);
+  va_end(list);
+  return result;
+}
+
+SYLVA_EXTERN sylva_value sylva_v_call(sylva_value context, sylva_func_id func_id, sylva_index length, va_list list) {
+  sylva_func func = sylva_func_resolve(context, func_id);
+  if (func != NULL) {
+    return sylva_func_v_call(func, context, length, list);
+  }
+  return sylva_value_nil;
+}
+
 char *sylva_func_id_registry_get_func_name(sylva_func_id_registry registry, sylva_func_id func_id) {
-  for (sylva_index i = 0; i < registry.capacity; i++) {
+  for (sylva_index i = 0; i < registry.length; i++) {
     if (registry.func_ids[i] == func_id) {
       return registry.func_names[i];
     }
@@ -56,7 +163,7 @@ char *sylva_func_id_registry_get_func_name(sylva_func_id_registry registry, sylv
 }
 
 sylva_func_id sylva_func_id_registry_get_func_id(sylva_func_id_registry registry, char *func_name) {
-  for (sylva_index i = 0; i < registry.capacity; i++) {
+  for (sylva_index i = 0; i < registry.length; i++) {
     if (strcmp(registry.func_names[i], func_name) == 0) {
       return registry.func_ids[i];
     }
@@ -73,7 +180,7 @@ sylva_func_id sylva_runtime_func_name_to_id(char *func_name) {
 }
 
 char *sylva_member_id_registry_get_func_name(sylva_member_id_registry registry, sylva_member_id member_id) {
-  for (sylva_index i = 0; i < registry.capacity; i++) {
+  for (sylva_index i = 0; i < registry.length; i++) {
     if (registry.member_ids[i] == member_id) {
       return registry.member_names[i];
     }
@@ -82,7 +189,7 @@ char *sylva_member_id_registry_get_func_name(sylva_member_id_registry registry, 
 }
 
 sylva_member_id sylva_member_id_registry_get_func_id(sylva_member_id_registry registry, char *member_name) {
-  for (sylva_index i = 0; i < registry.capacity; i++) {
+  for (sylva_index i = 0; i < registry.length; i++) {
     if (strcmp(registry.member_names[i], member_name) == 0) {
       return registry.member_ids[i];
     }
